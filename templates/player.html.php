0da5290ea0ab55b41b3cffe21fb5b45dae3d5e6b
quynhhuong <hh3009@nyu.edu>
2024-12-01 20:03:06 -0500

    Modified Team

--- templates/player.html.php
+++ templates/player.html.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<title></title>
+	<title>Players</title>
 </head>
 <body>
     <div class="navbar">
