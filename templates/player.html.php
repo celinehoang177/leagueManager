30556eed39da682f6206fab031491ae5598d9041
Nina Li <nl2539@nyu.edu>
2024-12-01 19:35:20 -0500

    Added UI

---
+++ templates/player.html.php
@@ -0,0 +1,16 @@
+<?php include __DIR__ . '/header.php'; ?>
+<head>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title></title>
+</head>
+<body>
+    <div class="navbar">
+        <a href="/leagueManager/myteam.php">My Team</a>
+        <a href="/leagueManager/myleague.php">My League</a>
+        <a href="/leagueManager/player.php">Players</a>
+        <a href="/leagueManager/trade.php">Trade</a>
+    </div>
+</body>
+<?php include __DIR__ . '/footer.php'; ?>
